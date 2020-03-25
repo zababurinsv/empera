b5b389db318c2b690ddad481f169508ada4570fa
progr76@gmail.com <progr76@gmail.com>
2020-03-25 19:23:32 +0300

    1.1605

--- Source/core/constant.js
+++ Source/core/constant.js
@@ -8,7 +8,7 @@
  * Telegram:  https://t.me/terafoundation
 */
 
-global.UPDATE_CODE_VERSION_NUM = 1604;
+global.UPDATE_CODE_VERSION_NUM = 1605;
 global.MIN_CODE_VERSION_NUM = 1364;
 global.MINING_VERSION_NUM = 0;
 
